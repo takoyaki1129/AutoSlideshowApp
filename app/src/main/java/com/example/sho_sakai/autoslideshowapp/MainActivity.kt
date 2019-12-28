1a7d6263ad75f64f31470c2e0b07829c775a0a58
Sho_SAKAI <sakaisho_1984@yahoo.co.jp>
2019-12-28 15:32:06 +0900

    Slideshow App

--- app/src/main/java/com/example/sho_sakai/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/sho_sakai/autoslideshowapp/MainActivity.kt
@@ -8,14 +8,15 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Handler
 import java.util.*
 
-class MainActivity: AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val PERMISSIONS_REQUEST_CODE = 100
+    private val PERMISSIONS_REQUEST_CODE = 10
 
     //タイマー
     private var mTimer: Timer? = null
@@ -25,6 +26,8 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
 
     private var mHandler = Handler()
 
+    var cursor: Cursor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,7 +35,6 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
         prev.setOnClickListener(this)
         play.setOnClickListener(this)
         forward.setOnClickListener(this)
-        pause.setOnClickListener(this)
 
 
         // Android 6.0以降の場合
@@ -56,24 +58,29 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
             PERMISSIONS_REQUEST_CODE ->
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     getContentsInfo()
+                    Log.d("ANDROID", "許可された")
+                } else {
+                    Log.d("ANDROID", "許可されなかった")
+                    return
                 }
         }
+
     }
 
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
+        cursor = resolver.query(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
                 null, // 項目(null = 全項目)
                 null, // フィルタ条件(null = フィルタなし)
                 null, // フィルタ用パラメータ
                 null // ソート (null ソートなし)
         )
-        if (cursor!!.moveToFirst()){
+        if (cursor!!.moveToFirst()) {
             // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
             val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             imageView.setImageURI(imageUri)
@@ -82,64 +89,79 @@ class MainActivity: AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View) {
-        when(v.id) {
-             R.id.prev -> {
-                if(cursor.isLast()) {
+        when (v.id) {
+            R.id.prev -> {
+                if (cursor!!.isFirst()) {
                     cursor!!.moveToLast()
+                } else {
+                    cursor!!.moveToPrevious()
                 }
-                cursor!!.moveToPrevious()
-                    val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                    val id = cursor.getLong(fieldIndex)
-                    val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                    imageView.setImageURI(imageUri)
-                }
+                imageView.setImageURI(imageUri)
+            }
 
             R.id.play -> {
                 if (mTimer == null) {
                     mTimer = Timer()
                     mTimer!!.schedule(object : TimerTask() {
                         override fun run() {
-                            cursor!!.moveToNext()
                             mHandler.post {
-                                timer.text = String.format("%.1f", mTimerSec)
+                                if (cursor!!.isLast()) {
+                                    cursor!!.moveToFirst()
+                                } else {
+                                    cursor!!.moveToNext()
+                                }
+                                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                                val id = cursor!!.getLong(fieldIndex)
+                                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                                imageView.setImageURI(imageUri)
+
                             }
                         }
                     }, 2000, 2000) // 最初に始動させるまで 2000ミリ秒、ループの間隔を 2000ミリ秒 に設定
 
-                    fun showHide() {
-                        play.visibility = View.INVISIBLE// 非表示にするメソッドが不明
-                        pause.visibility = View.VISIBLE
-                        }
-                    }
-                }
+                    prev.isEnabled = false
+                    forward.isEnabled = false
+                    play.text = "停止"
 
-            R.id.pause -> {
-                if (mTimer != null) {
-                    mTimer!!.cancel()
-                    mTimer = null
 
-                    fun showHide() {
-                        play.visibility = View.VISIBLE// 表示にするメソッドが不明
-                        pause.visibility = View.INVISIBLE
+                } else {
+                    if (mTimer != null) {
+                        mTimer!!.cancel()
+                        mTimer = null
                     }
+
+                    prev.isEnabled = true
+                    forward.isEnabled = true
+                    play.text = "再生"
                 }
             }
 
+
             R.id.forward -> {
-                if(cursor.isLast()) {
+                if (cursor!!.isLast()) {
                     cursor!!.moveToFirst()
+                } else {
+                    cursor!!.moveToNext()
                 }
-                cursor!!.moveToNext()
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
+                val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor!!.getLong(fieldIndex)
                 val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
                 imageView.setImageURI(imageUri)
             }
-            }
-            cursor.close()
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (cursor!! != null)
+            cursor!!.close()
+    }
+}
 
 
